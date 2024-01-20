060c816c7a1dfdebd92704d68cc11938d7eb566d
ilsanto29 <ilsanto29@localhost.localdomain>
2024-01-20 20:50:56 +0100

    first import

---
+++ src/Paypal.php
@@ -0,0 +1,26 @@
+<?php
+namespace Paypal\Sdk;
+
+/**
+ *
+ * @author PUTTINI2
+ *        
+ */
+class Paypal
+{
+
+    // TODO - Insert your code here
+
+    /**
+     */
+    public function __construct()
+    {
+
+        // TODO - Insert your code here
+    }
+    
+    public function prova() {
+        echo "prova";
+    }
+}
+
